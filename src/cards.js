0fa03c51ac10d349b2fb4aa7fe595346c62ccb58
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-11-29 13:21:31 +0200

    add-some-movies

--- src/cards.js
+++ src/cards.js
@@ -2,63 +2,74 @@ const cards = [
     {
         key: 1,
         title: "Schindler's List",
-        content:
-            ""
+        content: ""
     },
     {
         key: 2,
         title: "The Lord Of The Rings: The Return Of The King",
-        content:
-            ""
+        content: ""
     },
     {
         key: 3,
         title: "Pulp Fiction",
-        content:
-            ""
+        content: ""
     },
     {
         key: 4,
         title: "The Good, The Bad And The Ugly",
-        content:
-            ""
+        content: ""
     },
     {
         key: 5,
         title: "The Lord Of The Rings: The Fellowship Of The Ring",
-        content:
-            ""
+        content: ""
     },
     {
         key: 6,
         title: "Fight Club",
-        content:
-            ""
+        content: ""
     },
     {
         key: 7,
         title: "Forrest Gump",
-        content:
-            ""
+        content: ""
     },
     {
         key: 8,
         title: "Inception ",
-        content:
-            ""
+        content: ""
     },
     {
         key: 9,
         title: "Star Wars: Episode V - The Empire Strikes Back",
-        content:
-            ""
+        content: ""
     },
     {
         key: 10,
         title: "The Lord Of The Rings: The Two Towers",
-        content:
-            ""
+        content: ""
+    }, {
+        key: 11,
+        title: "Angry Men",
+        content: ""
+    }, {
+        key: 12,
+        title: "The Dark Knight",
+        content: ""
+    }, {
+        key: 13,
+        title: "The Godfather: Part II",
+        content: ""
+    }, {
+        key: 14,
+        title: "The Godfather",
+        content: ""
+    }, {
+        key: 15,
+        title: "The Shawshank Redemption",
+        content: ""
     }
+
 ];
 
 export default cards;
