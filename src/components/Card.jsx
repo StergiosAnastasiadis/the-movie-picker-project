84f3a8619a7c1f793753a2a776b5f2b5d47ba11f
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-11-29 15:26:46 +0200

    fix-some-styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const Card = (props) => {
 
     return (
-        <div className="card" id="movie-cards">
-            <a className="add-movie-button" href="#">Add</a>
-            <img alt="movie-poster"></img>
-            <h1>{props.title} </h1>
+        <div className="movie-cards">
+            <div className="card" id="movie-cards">
+                <a className="add-movie-button" href="#">Add</a>
+                <img alt="movie-poster"></img>
+                <h1>{props.title} </h1>
+            </div>
         </div>
     )
 }
