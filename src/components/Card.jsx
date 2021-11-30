00503d35a48d7e7464cb42a355b2daa88a126445
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-11-30 14:20:52 +0200

    some-changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card = (props) => {
+const Card = (movie) => {
 
     return (
         <div>
@@ -9,8 +9,8 @@ const Card = (props) => {
                 <button id="add-movie-button" className="btn btn-secondary btn-sm" >Add</button>
                 {/* <img alt="movie-poster"></img> | DONE*/}
                 {/* Hint - img tag is self-closing Tag | DONE*/}
-                <img src={props.imgSrc} alt="movie-poster" />
-                <h1>{props.title} </h1>
+                <img src={movie.imgSrc} alt="movie-poster" />
+                <h1>{movie.title} </h1>
             </div>
         </div>
     )
