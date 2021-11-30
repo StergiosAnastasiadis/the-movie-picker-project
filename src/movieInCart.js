3b2ea19639b862981504083e00b8e40b7a43de38
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-11-30 11:56:30 +0200

    change-name

---
+++ src/movieInCart.js
@@ -0,0 +1,20 @@
+const movieInCart = [
+    {
+        key: 1,
+        title: "Schindler's List",
+        content: ""
+    }, {
+        key: 4,
+        title: "The Good, The Bad And The Ugly",
+    }, {
+        key: 7,
+        title: "Forrest Gump",
+    },
+    {
+        key: 10,
+        title: "The Lord Of The Rings: The Two Towers",
+    }
+
+];
+
+export default movieInCart;
