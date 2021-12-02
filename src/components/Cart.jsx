60d54d92f7c69c567f0ea3bd88ecc4cbca693e81
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-12-02 14:14:18 +0200

    show-movies-inside-cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,40 +1,15 @@
-import React from "react";
-import moviesInCart from "../moviesInCart";
+import React, { useState } from "react";
 
-const Cart = ({ setMoviesInsideCart }) => {
-
-    const createCart = (movie) => {
-        return (
-            <div className="buy-list" key={movie.id}>
-                <p >{movie.title} <button id="remove-item-button" className="btn" value={movie.id} onClick={deleteMovie}>Remove</button></p>
-            </div>
-        )
-    }
+const Cart = ({ id, title, setMoviesInsideCart }) => {
 
     const deleteMovie = (event) => {
-        //Remove Movie from moviesInCart Array
-        const movieToDelete = event.target.value;
+        //Remove Movie from moviesInsideCart Array
 
-        for (var i = 0; i < moviesInCart.length; i++) {
-            var obj = moviesInCart[i];
-
-            if (movieToDelete.indexOf(obj.id) !== -1) {
-                moviesInCart.splice(i, 1);
-                i--;
-            }
-        }
-        console.log(moviesInCart);
     }
 
     return (
-        <div className="movie-cart">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Buy Movies</h5>
-                    {moviesInCart.map(createCart)}
-                    <button className="btn btn-outline-warning">Buy Button</button>
-                </div>
-            </div>
+        <div className="buy-list" key={id}>
+            <p >{title} <button id="remove-item-button" className="btn" value={id} >Remove</button></p>
         </div>
     )
 }
