9ec7a3c0c44ae433d27bbe48b962564b32f11cbf
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-12-02 14:12:13 +0200

    fix-Cart-Component

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ function App() {
   // edw tha mpoun ta state tou component
   const [userSearchInput, setUserSearchInput] = useState("");
   const [data, setData] = useState([]);
-  const [moviesInsideCart, setMoviesInsideCart] = useState([{}]);
+  const [moviesInsideCart, setMoviesInsideCart] = useState([]);
 
   const url = `https://api.themoviedb.org/3/search/movie?api_key=386f116d61a5532dc4337deb9a45133c&language=en-US&query=${userSearchInput}&page=1&include_adult=false`;
 
   useEffect(() => {
     userSearchInput && getData();
   }, [userSearchInput]);
+
+  useEffect(() => {
+    console.log(moviesInsideCart);
+  }, [moviesInsideCart]);
 
   const getData = () => {
 
@@ -32,15 +36,24 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
 
+      <Header />
       <Input setUserSearchInput={setUserSearchInput} />
 
       <div className="add_your_flex_styling_check_wrap">
-        {data.map((item) => (<Card key={item.id} id={item.id} title={item.title} poster_path={item.poster_path} moviesInsideCart={moviesInsideCart} setMoviesInsideCart={setMoviesInsideCart} />))}
+        {data.map((item) => (<Card key={item.id} id={item.id} title={item.title} poster_path={item.poster_path} setMoviesInsideCart={setMoviesInsideCart} />))}
       </div>
 
-      <Cart setMoviesInsideCart={setMoviesInsideCart} />
+      <div className="movie-cart">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Buy Movies</h5>
+            {moviesInsideCart.map((item) => (<Cart key={item.id} id={item.id} title={item.title} setMoviesInsideCart={setMoviesInsideCart} />))}
+            <button className="btn btn-outline-warning">Buy Button</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
