a919f708f5dcca6957d7169a24237ef50c9710b6
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-12-02 11:14:18 +0200

    add-some-vars

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   // edw tha mpoun ta state tou component
   const [userSearchInput, setUserSearchInput] = useState("");
   const [data, setData] = useState([]);
+  const [moviesInsideCart, setMoviesInsideCart] = useState([{}]);
 
   const url = `https://api.themoviedb.org/3/search/movie?api_key=386f116d61a5532dc4337deb9a45133c&language=en-US&query=${userSearchInput}&page=1&include_adult=false`;
 
@@ -36,10 +37,10 @@ function App() {
       <Input setUserSearchInput={setUserSearchInput} />
 
       <div className="add_your_flex_styling_check_wrap">
-        {data.map((item) => (<Card key={item.id} id={item.id} title={item.title} poster_path={item.poster_path} />))}
+        {data.map((item) => (<Card key={item.id} id={item.id} title={item.title} poster_path={item.poster_path} moviesInsideCart={moviesInsideCart} setMoviesInsideCart={setMoviesInsideCart} />))}
       </div>
 
-      <Cart />
+      <Cart setMoviesInsideCart={setMoviesInsideCart} />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import moviesInCart from "../moviesInCart";
 
-const Card = (movie) => {
+const Card = (item, { moviesInsideCart, setMoviesInsideCart }) => {
 
     const imgUrl = "https://image.tmdb.org/t/p/original/";
 
     const addMovieButton = () => {
-        // Add Movie to moviesInCart Array
-        console.log(movie.id);
-        console.log(movie.title);
-        moviesInCart.push({ id: movie.id, title: movie.title });
+        // Add Movie in moviesInCart Fake Array
+
+        const obj = { id: item.id, title: item.title };
+        moviesInCart.push(obj);
+        console.log(obj);
         console.log(moviesInCart);
+
+        // Try to add 
+        //setMoviesInsideCart([...moviesInsideCart, obj]);
+        //setMoviesInsideCart(moviesInsideCart => [...moviesInsideCart, obj])    
     }
 
     return (
         <div>
             <div className="card" id="movie-cards">
-                <button id="add-movie-button" className="btn btn-secondary btn-sm" value={movie.id} onClick={addMovieButton}>Add</button>
-                <img src={imgUrl + movie.poster_path} alt="movie-poster" />
-                <h1>{movie.title} </h1>
+                <button id="add-movie-button" className="btn btn-secondary btn-sm" onClick={addMovieButton}>Add</button>
+                <img src={imgUrl + item.poster_path} alt="movie-poster" />
+                <h1>{item.title} </h1>
             </div>
         </div>
     )
