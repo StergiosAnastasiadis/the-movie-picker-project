a292bb62a2e997512d8347f96c32315df70b4e2a
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-12-02 14:13:39 +0200

    add-movies-into-cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,23 @@
 import React, { useState } from "react";
-import moviesInCart from "../moviesInCart";
 
-const Card = (item, { moviesInsideCart, setMoviesInsideCart }) => {
+const Card = ({ id, title, poster_path, setMoviesInsideCart }) => {
 
     const imgUrl = "https://image.tmdb.org/t/p/original/";
 
     const addMovieButton = () => {
-        // Add Movie in moviesInCart Fake Array
 
-        const obj = { id: item.id, title: item.title };
-        moviesInCart.push(obj);
+        const obj = { id: id, title: title, poster_path: poster_path };
         console.log(obj);
-        console.log(moviesInCart);
 
-        // Try to add 
-        //setMoviesInsideCart([...moviesInsideCart, obj]);
-        //setMoviesInsideCart(moviesInsideCart => [...moviesInsideCart, obj])    
+        setMoviesInsideCart((moviesInsideCart) => [...moviesInsideCart, obj]);
     }
 
     return (
         <div>
             <div className="card" id="movie-cards">
                 <button id="add-movie-button" className="btn btn-secondary btn-sm" onClick={addMovieButton}>Add</button>
-                <img src={imgUrl + item.poster_path} alt="movie-poster" />
-                <h1>{item.title} </h1>
+                <img src={imgUrl + poster_path} alt="movie-poster" />
+                {<h1>{title} </h1>}
             </div>
         </div>
     )
