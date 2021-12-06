d47fbcb2c4012bd3eeba4482446dbe5eaeb802e7
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-06 12:15:36 +0200

    add-Purchase-Functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Input from "./components/Input";
-import Cart from "./components/Cart";
 import Card from "./components/Card";
+import Cart from "./components/Cart";
+import defaultMovieCards from "./defaultMovieCards";
 import NoMoviesFound from "./components/NoMoviesFound";
 import getData from "./getData";
-import defaultMovieCards from "./defaultMovieCards";
+import purchaseMovies from "./purchaseMovies";
 import "./App.css";
 
 const App = () => {
@@ -13,7 +14,6 @@ const App = () => {
   const [userSearchInput, setUserSearchInput] = useState("");
   const [data, setData] = useState(defaultMovieCards);
   const [moviesInsideCart, setMoviesInsideCart] = useState([]);
-
 
   useEffect(() => {
     userSearchInput && getData(userSearchInput).then(res => setData(res));   
@@ -30,6 +30,16 @@ const App = () => {
     setMoviesInsideCart((moviesInsideCart) => [...moviesInsideCart, card]);
   }
 
+  const purchaseSuccess = () => {
+    setMoviesInsideCart([]);
+    console.log("successfully purchased");
+  }
+
+  const buyMoviesButton = () => {
+    purchaseMovies(moviesInsideCart).then(res => {
+      res.data.success ? purchaseSuccess() : console.log("Error");
+    })
+  }
 
   return (
     <div className="App">
@@ -44,7 +54,7 @@ const App = () => {
       <div className="movie-cart">
         <h5 className="card-title">Buy Movies</h5>
         {moviesInsideCart && moviesInsideCart.map((item) => (<Cart key={item.id} id={item.id} title={item.title} setMoviesInsideCart={setMoviesInsideCart} />))}
-        <button className="btn btn-outline-warning" id="buy-button">Buy Button</button>
+        <button className="btn btn-outline-warning" id="buy-button" onClick={buyMoviesButton}>Buy Button</button>
       </div>
     </div>
   );
---
+++ src/purchaseMovies.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const purchaseMovies = (moviesInsideCart) => {
+
+    if(moviesInsideCart === []) return;
+
+    const url = "https://api.mocklets.com/mock68075/";
+    const config = { headers: {"X-Mocklets-PublicKey" : "txmovies", "X-Mocklets-Checksum" : "830c7cd4a70be6540a4898441ca02951"}};
+    let moviesToBuy ={data:{movies:[]}};
+
+    moviesInsideCart.map((movie) => {
+        moviesToBuy.data.movies.push(movie.id);
+        return moviesToBuy;
+    })
+    
+    return axios.post(url, moviesToBuy, config)
+        .then(res => res)
+        .catch(err => console.log(err));
+
+}
+
+export default purchaseMovies;
