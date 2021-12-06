30c2d4d62c8dd037514edeaacd722e92c50adf30
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-06 19:35:02 +0200

    fix-the-get-call

--- src/getData.js
+++ src/getData.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-const getData = (userSearchInput) => {
+const getData = async (userSearchInput, setIsLoading) => {
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=386f116d61a5532dc4337deb9a45133c&language=en-US&query=${userSearchInput}&page=1&include_adult=false`;
 
-    return axios
+    setIsLoading(true);
+    return await axios
         .get(url)
-        .then(res => res.data.results)
+        .then((res) => {
+            setIsLoading(false)
+            return res.data.results 
+        })
         .catch(err => console.log(err));
 };
 
