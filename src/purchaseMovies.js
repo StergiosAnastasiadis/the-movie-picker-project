697eb1f22afd05cd1a9f003924dc98862afb94d8
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-06 12:28:49 +0200

    remove-line

--- src/purchaseMovies.js
+++ src/purchaseMovies.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const purchaseMovies = (moviesInsideCart) => {
 
-    if(moviesInsideCart === []) return;
-
     const url = "https://api.mocklets.com/mock68075/";
     const config = { headers: {"X-Mocklets-PublicKey" : "txmovies", "X-Mocklets-Checksum" : "830c7cd4a70be6540a4898441ca02951"}};
     let moviesToBuy ={data:{movies:[]}};
