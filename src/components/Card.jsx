602e23c79ce94ac508c9df9ee6ed652a1ac47762
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-06 12:39:59 +0200

    remove-404-errors-if-img-doesnt-exist

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ id, title, poster_path, buttonDisable, addMovieButton }) => {
         <div>
             <div className="card" id="movie-cards">
                 <button id="add-movie-button" className="btn btn-secondary btn-sm" onClick={addMovieButton} value={id} disabled={buttonDisable}>Add</button>
-                <img src={imgUrl + poster_path} alt="movie-poster" />
+                <img src={poster_path === null ? "" :imgUrl + poster_path} alt="movie-poster" />
                 {<h1>{title} </h1>}
             </div>
         </div>
