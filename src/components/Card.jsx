a1dba877dfd76a6cf12b1b528308eafd49eb61ec
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-08 15:16:42 +0200

    Change-button-from-add-to-remove- & vice versa

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { imgUrl } from "../urlStrings";
 
-const Card = ({ id, title, poster_path, buttonDisable, addMovieButton }) => {
+const Card = ({ id, title, poster_path, buttonDisable, addMovieButton, setMoviesInsideCart }) => {
+
+    const deleteMovie = () => {
+        setMoviesInsideCart((moviesInsideCart) => {
+            const newArray = moviesInsideCart.filter(movie => !(movie.id === id));
+            return newArray
+        })
+    }
     
     return (
         <div>
             <div className="card" id="movie-cards">
-                <button id="add-movie-button" className="btn btn-secondary btn-sm" onClick={addMovieButton} value={id} disabled={buttonDisable}>Add</button>
+                <button id="add-movie-button" className="btn btn-secondary btn-sm" onClick={buttonDisable ? deleteMovie : addMovieButton} value={id} > {buttonDisable ? "Remove" : "Add"}</button>
                 <img src={poster_path === null ? "" :imgUrl + poster_path} alt="movie-poster" />
                 {<h1>{title} </h1>}
             </div>
         </div>
     )
 }
-
+//disabled={buttonDisable}
 export default Card;
