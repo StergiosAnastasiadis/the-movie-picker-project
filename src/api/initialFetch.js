1ab54de7137cda2767147bbb20e5fda78e314e36
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-10 12:33:32 +0200

    handle-error-of-initial-fetch

--- src/api/initialFetch.js
+++ src/api/initialFetch.js
@@ -4,7 +4,7 @@ const initialFetch = ()  => {
     const url = "https://api.themoviedb.org/3/movie/popular?api_key=386f116d61a5532dc4337deb9a45133c&language=en-US&page=1"
     
     return axios.get(url)
-    .then(res => res.data.results)
+    .then(res => res)
     .catch(err => console.log(err));
 }
 
