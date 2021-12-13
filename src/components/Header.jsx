71fa9668d0aecb51c2970be8b871379c4b150c9d
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-13 17:03:15 +0200

    add-sign-in-link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import DataContext from "../context/DataContext";
 
 const Header = () => {
+  const { moviesInsideCart } = useContext(DataContext);
 
-    const { moviesInsideCart }  =  useContext(DataContext);
-
-    return (
-        <header>
-            <h1>You have selected {moviesInsideCart.length} Movies</h1>
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1 className="header-title">You have selected {moviesInsideCart.length} Movies</h1>
+      <Link to="/auth/login">
+        <button className="btn btn-outline-dark header-button">Sign In</button>
+      </Link>
+    </header>
+  );
+};
 
 export default Header;
