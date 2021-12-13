5a8270d552a0e6b6b618166e76fb618326b51642
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-13 17:03:59 +0200

    remove-id-from-html

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,20 +7,20 @@ const Input = () => {
   const { lowestToHighest, highestToLowest, handleUserSearchInput } = useContext(DataContext);
 
   return (
-      <div className="input-group mb-3">
-        <input className="form-control" id="input-field" type="text" placeholder="Search for movies..." autoComplete="off" autoFocus="autofocus" onChange={handleUserSearchInput} />
-        <div className="input-group-append">
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Order By
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <button className="btn btn-light dropdown-item" onClick={highestToLowest}>Highest</button>
-              <button className="btn btn-light dropdown-item" onClick={lowestToHighest}>Lowest</button>
-            </div>
+    <div className="input-group mb-3">
+      <input className="form-control input-field" type="text" placeholder="Search for movies..." autoComplete="off" autoFocus="autofocus" onChange={handleUserSearchInput} />
+      <div className="input-group-append">
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Order By
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button className="btn btn-light dropdown-item" onClick={highestToLowest}>Highest</button>
+            <button className="btn btn-light dropdown-item" onClick={lowestToHighest}>Lowest</button>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
