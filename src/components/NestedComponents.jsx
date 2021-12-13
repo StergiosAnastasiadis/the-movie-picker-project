1a61d19ccdca13b72b0bcb82398ad433437951f2
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-13 17:07:06 +0200

    format-file

--- src/components/NestedComponents.jsx
+++ src/components/NestedComponents.jsx
@@ -6,24 +6,24 @@ import Cart from "./Cart";
 
 const NestedComponents = () => {
 
-    const { userSearchInput, moviesInsideCart, buyMoviesButton, data, isLoading, addMovieButton } = useContext(DataContext);
+  const { userSearchInput, moviesInsideCart, buyMoviesButton, data, isLoading, addMovieButton } = useContext(DataContext);
 
-    return (
-        <div className="flex-container">
-        {
+  return (
+    <div className="flex-container">
+      {
         (userSearchInput && data.length === 0) ? <h1>No Movies Found</h1>
-        :(isLoading) ? <Loading />
-        :<div className="movie-cards-container">
-        {data.map((item) => (<Card item={item} key={item.id} buttonStatus={moviesInsideCart.some(mic => mic.id === item.id)} addMovieButton={() => addMovieButton({...item})} />))}
-        </div>
-        }
-        <div className="movie-cart">
-          <h5 className="card-title">Buy Movies</h5>
-          {moviesInsideCart && moviesInsideCart.map((item) => (<Cart item={item} key={item.id} />))}
-          <button className="btn btn-outline-warning" id="buy-button" onClick={buyMoviesButton}>Purchase</button>
-        </div>
-        </div>
-    )
+          : (isLoading) ? <Loading />
+            : <div className="movie-cards-container">
+              {data.map((item) => (<Card item={item} key={item.id} buttonStatus={moviesInsideCart.some(mic => mic.id === item.id)} addMovieButton={() => addMovieButton({ ...item })} />))}
+            </div>
+      }
+      <div className="movie-cart">
+        <h5 className="card-title">Buy Movies</h5>
+        {moviesInsideCart && moviesInsideCart.map((item) => (<Cart item={item} key={item.id} />))}
+        <button className="btn btn-outline-warning buy-button" onClick={buyMoviesButton}>Purchase</button>
+      </div>
+    </div>
+  )
 }
 
 export default NestedComponents;
