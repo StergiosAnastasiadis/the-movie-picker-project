83dc7671721d06fa3488f9206f9b5feed792a178
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-14 14:47:31 +0200

    double-quotes-4all

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 import { DataProvider } from "../context/DataContext";
-import LoginHeader from './auth/LoginHeader';
-import LoginForm from './auth/LoginForm';
+import LoginHeader from "./auth/LoginHeader";
+import LoginForm from "./auth/LoginForm";
 
 const Auth = () => {
     return(
