264cd3ce52735cb59a3f2a94140b9ace8ee2b48c
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-16 21:12:54 +0200

    separate-Cart-Component-from-NestedComponents.jsx

--- src/components/home/Cart.jsx
+++ src/components/home/Cart.jsx
@@ -1,24 +1,51 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import DataContext from "../../context/DataContext";
+import purchaseMovies from "../../api/purchaseMovies";
+import { toast } from "react-toastify";
 
-const Cart = ({ item }) => {
+const Cart = () => {
 
-    const { setMoviesInsideCart } = useContext(DataContext);
+    const { setMoviesInsideCart, moviesInsideCart, isAuth, userName } = useContext(DataContext);
 
-    const deleteMovie = () => {
+    const [isBuyButtonClicked, setIsButtonClicked] = useState(false);
+
+    const purchaseMoviesInsideCart = async () => {
+        if (moviesInsideCart.length === 0 || isBuyButtonClicked) { return; }
+        if (!isAuth) { return toast.warning("You have to be logged in to purchase movies") }
+        setIsButtonClicked(true);
+        purchaseMovies(moviesInsideCart)
+            .then(res => purchaseSuccess(res))
+            .catch(err => { toast.error("Failed to Purchase Movies, Please Try Again."); setIsButtonClicked(false); console.log(err) })
+    }
+
+    const notifySuccess = () => toast.success(`Congratulations ${userName} you purchased ${moviesInsideCart.length} Movie(s)`);
+
+    const purchaseSuccess = (res) => {
+        setMoviesInsideCart([]);
+        setIsButtonClicked(false);
+        notifySuccess();
+        console.log(res.config.data);
+    };
+
+    const deleteMovie = (id) => {
         setMoviesInsideCart((moviesInsideCart) => {
-            const newArray = moviesInsideCart.filter(movie => !(movie.id === item.id));
+            const newArray = moviesInsideCart.filter(movie => !(movie.id === id));
             return newArray
         })
     }
 
     return (
-        <div className="buy-list">
-            <div className="cart-movie-title"><p >{item.title} </p></div>
-            <div className="cart-movie-remove-button">
-            <button className="btn remove-item-button" onClick={deleteMovie}>Remove</button></div>
-        </div>
+            <div className="movie-cart">
+                <h5 className="card-title">Buy Movies</h5>
+                {moviesInsideCart && moviesInsideCart.map(item => (
+                    <div className="buy-list" key={item.id}>
+                        <div className="cart-movie-title"><p >{item.title} </p></div>
+                        <div className="cart-movie-remove-button" >
+                            <button className="btn remove-item-button" onClick={() => deleteMovie(item.id)}>Remove</button></div>
+                    </div>
+                ))}
+                <button className="btn btn-outline-warning buy-button" onClick={purchaseMoviesInsideCart}>Purchase</button>
+            </div>
     )
 }
 
