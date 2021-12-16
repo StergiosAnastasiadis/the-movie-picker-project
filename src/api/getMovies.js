6868bb56dac67612aad5bb58812b1e1b3846b0a7
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-16 15:20:25 +0200

    remove-unnecessary-code-from-getRequests

--- src/api/getMovies.js
+++ src/api/getMovies.js
@@ -5,13 +5,8 @@ const getMovies = async (userMovieSearch, setIsLoading) => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=386f116d61a5532dc4337deb9a45133c&language=en-US&query=${userMovieSearch}&page=1&include_adult=false`;
 
     setIsLoading(true);
-    return await axios
-        .get(url)
-        .then((res) => {
-            setTimeout(() => { setIsLoading(false) }, 500);
-            return res.data.results
-        })
-        .catch(err => console.log(err));
+    setTimeout(() => { setIsLoading(false) }, 500);
+    return await axios.get(url)
 };
 
 export default getMovies;
