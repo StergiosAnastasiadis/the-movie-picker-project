a7726e7b8d8c8d00a24d163c585249188109c63e
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-22 17:48:51 +0200

    write on one line

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -6,10 +6,8 @@ import { ToastContainer } from "react-toastify";
 
 const Auth = () => {
 
-    let storedAuthStatus = localStorage.getItem("Auth");
-
     return (
-        storedAuthStatus === "true"
+        localStorage.getItem("Auth") === "true"
             ?
             <h1>You are already Logged In</h1>
             :
