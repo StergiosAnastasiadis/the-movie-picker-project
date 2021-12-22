7e76cf41cbe2c59e62296f06dd24b1c989d58e2e
Stergios Anastasiadis <Stergios.Anastasiadis@arhs-dev-hellas.com>
2021-12-22 16:30:10 +0200

    Write setPassword function inside on Change. And prevent space input in user name

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import DataContext from "../../context/DataContext";
 import { toast } from "react-toastify";
@@ -7,15 +7,11 @@ const LoginForm = () => {
 
     const { userName, setUserName } = useContext(DataContext);
     const [userPassword, setUserPassword] = useState("");
-
+        
     let navigate = useNavigate();
 
-    const handleUserLoginName = (event) => {
-        setUserName(event.target.value);
-    }
-
-    const handleUserPassword = (event) => {
-        setUserPassword(event.target.value);
+    const userNameValidation = (e) => {
+        setUserName(e.target.value.replace(/\s+/g, ''));
     }
 
     const handleUserLogin = (event) => {
@@ -32,11 +28,11 @@ const LoginForm = () => {
             <form>
                 <div className="form-group">
                     <label>User Name</label>
-                    <input type="text" className="form-control" placeholder="Enter User Name" autoFocus="autofocus" onChange={handleUserLoginName} />
+                    <input type="text" className="form-control" placeholder="Enter User Name" autoFocus="autofocus" value={userName} onChange={userNameValidation} />
                 </div>
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Password" onChange={handleUserPassword} />
+                    <input type="password" className="form-control" placeholder="Password" onChange={(e) => setUserPassword(e.target.value)} />
                 </div>
                 <button type="submit" className="btn btn-warning login-sign-in-button" onClick={handleUserLogin}>Sign In</button>
             </form>
